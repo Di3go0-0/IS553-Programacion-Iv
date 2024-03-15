3b42125146b67c3e12752828e3a0942de0941ef4
Juan Diego <D.rincon@utp.edu.co>
2024-03-15 08:43:06 -0500

    delete DBS

--- PoyFinal/BDS/prueba.java
+++
@@ -1,8 +0,0 @@
-package PoyFinal.BDS;
-
-public class prueba {
-    public static void main (String[] args){
-        Users users = new Users();
-        users.addUser("John Doe", "johndoe@example.com", "password123", "admin");
-    }
-}
