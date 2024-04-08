432dc5d0d8b6218958f05b4969f6b683bd163735
Juan Diego <D.rincon@utp.edu.co>
2024-04-07 22:47:37 -0500

    Start interface

---
+++ ReservasHotel/ReservasHotel/src/Menu/Menu.java
@@ -0,0 +1,40 @@
+package Menu;
+
+import Utils.InputHandler;
+import Users.UserManager;
+
+public class Menu {
+	private int option;
+    private UserManager userManager = new UserManager();
+    private InputHandler inputHandler = new InputHandler();
+
+    private void printOptions() {
+        inputHandler.printOptions();
+    }
+
+    private void scannerOptions() {
+        this.option = inputHandler.getOption();
+    }
+
+    public void menuLoop() {
+        while(this.option != 3){
+            printOptions();
+            scannerOptions();
+            switch (this.option) {
+                case 1:
+                    userManager.registerClient();
+                    break;
+                case 2:
+                    userManager.loginClient();
+                    break;
+                case 3:
+                    System.out.println("Saliendo...");
+                    this.option = 3;
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+    }
+}
---
+++ ReservasHotel/ReservasHotel/src/PoyFinal/ProyectoFinal.java
@@ -0,0 +1,10 @@
+package PoyFinal;
+
+import Menu.Menu;
+
+public class ProyectoFinal {
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.menuLoop();
+	}
+}
